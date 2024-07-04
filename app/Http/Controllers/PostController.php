fdd4dc4d10e4e85e6820d2d2aaf8e67fda3bdd73
Andrew McKenzie <amcken33@myune.edu.au>
2024-07-04 10:09:43 +1000

    Added blog views and routes for update, show, create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('index');
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function show(Post $post) {
+        return view('posts.show', compact('post'));
+    }
+
+    public function edit(string $id) {
+        //
+    }
+
+    public function update(Request $request, string $id) {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        Post::create($request->all());
+        return redirect()->route('posts.index');
+    }
+
+    public function store(Request $request) {
+        //
+    }
+
+    public function destroy(string $id) {
+        //
     }
 }
--- resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    This is the content for the index file
-@endsection
