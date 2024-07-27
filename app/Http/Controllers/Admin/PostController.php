9a52acb73da24b1ba68be8638b0c3172796d888b
Andrew McKenzie <amcken33@myune.edu.au>
2024-07-27 10:08:29 +1000

    Added middleware for admin and admin controllers

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function show(Post $post) {
+        return view('posts.show', compact('post'));
+    }
+
+    public function edit(Post $post) {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post) {
+        $request->validate([
+                    'title' => 'required',
+                    'content' => 'required',
+                ]);
+
+         $post->update($request->all());
+         return redirect()->route('posts.show', $post->id);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+                    'title' => 'required',
+                    'content' => 'required',
+                ]);
+
+        Post::create($request->all());
+        return redirect()->route('posts.index');
+    }
+
+    public function confirmDelete(Post $post) {
+        return view('posts.delete', compact('post'));
+    }
+
+    public function destroy(Post $post) {
+        $post->delete();
+        return redirect()->route('posts.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PostController as AdminPostController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +13,12 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('posts', PostController::class);
     Route::get('posts/{post}/delete', [PostController::class, 'confirmDelete'])->name('posts.delete');
     Route::get('/home', [PostController::class, 'index'])->name('home');
+});
+
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function() {
+    Route::get('/', [AdminPostController::class, 'index'])->name('admin.dashboard');
+    Route::resource('posts', AdminPostController::class, ['as' => 'admin']);
+    Route::resource('users', AdminUserController::class, ['as' => 'admin']);
 });
 
 Auth::routes();
